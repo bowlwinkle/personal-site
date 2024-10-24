506d78e9a4d91bed5689e92a25964e6636d0940c
Lucas Gansberg <lucas.gansberg@gmail.com>
2024-10-23 22:00:20 -0700

    Dark mode first pass

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -4,13 +4,23 @@ export function ContactInfo() {
   return (
     <List horizontal>
       <ListItem className="contactLink">
+        <a href="mailto:lucas.gansberg@gmail.com">
+          <Icon name="mail" />
+        </a>
+      </ListItem>
+      <ListItem className="contactLink">
+        <a href="www.lucas-gansberg.com">
+          <Icon name="user circle" />
+        </a>
+      </ListItem>
+      <ListItem className="contactLink">
         <a href="https://www.linkedin.com/in/lucas-gansberg/">
           <ListIcon name="linkedin" />
         </a>
       </ListItem>
       <ListItem className="contactLink">
-        <a href="mailto:lucas.gansberg@gmail.com">
-          <Icon name="mail" />
+        <a href="https://github.com/bowlwinkle">
+          <ListIcon name="github" />
         </a>
       </ListItem>
     </List>
