6bcc113ce70518792898a8cb7a9e2a9286f5419a
Lucas Gansberg <lucas.gansberg@gmail.com>
2024-12-05 13:14:44 -0800

    Adding popups for info icons

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,22 +1,34 @@
 import { NavLink } from 'react-router-dom'
-import { Icon, List, ListIcon, ListItem } from 'semantic-ui-react'
+import { Icon, List, ListIcon, ListItem, Popup } from 'semantic-ui-react'
 
 export function ContactInfo() {
   return (
     <List horizontal>
       <ListItem className="contactLink">
         <a href="https://www.linkedin.com/in/lucas-gansberg/">
-          <ListIcon name="linkedin" />
+          <Popup
+            trigger={<ListIcon name="linkedin" />}
+            content="LinkedIn"
+            position="bottom center"
+          />
         </a>
       </ListItem>
       <ListItem className="contactLink">
         <a href="mailto:lucas.gansberg@gmail.com">
-          <Icon name="mail" />
+          <Popup
+            trigger={<Icon name="mail" />}
+            content="Email"
+            position="bottom center"
+          />
         </a>
       </ListItem>
       <ListItem className="contactLink">
         <NavLink to="/work?tab=Resume">
-          <Icon name="file alternate" />
+          <Popup
+            trigger={<Icon name="file alternate" />}
+            content="Resume"
+            position="bottom center"
+          />
         </NavLink>
       </ListItem>
     </List>
